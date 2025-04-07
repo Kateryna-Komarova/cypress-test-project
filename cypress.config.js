eebe7df5ee7ce47828b6e382a1a264b3a2a06683
Kateryna-Komarova <svonsonket@gmail.com>
2025-04-07 13:10:43 +0200

    Create report

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    reporter: "mochawesome",
+    reporterOptions: {
+      reportDir: "cypress/reports/mochawesome",
+      overwrite: false,
+      html: true,
+      json: true,
     },
   },
 });
--- cypress/e2e/input/input.page.js
+++ cypress/e2e/input/input.page.js
@@ -1,21 +1,20 @@
-class InputPage{
-    clickInputLink(){
-        CSSSkewY.contains("Input").click();
-    }
-    getInputField(){
-        return cy.get('div.example input[type="number"]');
+class InputPage {
+  clickInputLink() {
+    cy.contains("Input").click();
+  }
+  getInputField() {
+    return cy.get('div.example input[type="number"]');
+  }
+  enterNumber(number) {
+    this.getInputField().type(number);
+  }
+  clearInputField() {
+    this.getInputField().clear();
+  }
 
-    }
-    enterNumber(number){
-        this.getInputField.type(number);
-    }
-    clearInputField(){
-        this.getInputField().clear();
-    }
-
-    assertInputValue(expectedValue){
-        this.getInputField.should("have.value", expectedValue);
-    }
+  assertInputValue(expectedValue) {
+    this.getInputField().should("have.value", expectedValue);
+  }
 }
 
 export default new InputPage();
